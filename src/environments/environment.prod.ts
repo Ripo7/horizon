e57734b158da6152b4c001f4a99a51d93d858d25
Doinsport <doinsport@MacBook-Pro.local>
2022-01-07 10:53:34 +0100

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,15 @@
 export const environment = {
-  production: true,
-  backUrl: 'http://13.38.51.28:3000'
+  production: false,
+  moralis: {
+    appId: 'v3lpBcMarcYd32SeINIz8BKdFUbUOyYGANsXEW9G',
+    serverUrl: 'https://63miqb59ssrn.usemoralis.com:2053/server'
+  },
+  firebaseConfig: {
+    apiKey: "AIzaSyC8QF5NpoUhbCTLRbs8gtr6P6xlhzPUie8",
+    authDomain: "horizon-15839.firebaseapp.com",
+    projectId: "horizon-15839",
+    storageBucket: "horizon-15839.appspot.com",
+    messagingSenderId: "516153081735",
+    appId: "1:516153081735:web:efdde198e9dff12a1d5d22"
+  }
 };
